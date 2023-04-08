ef805d15f012d6eee5454bb76fe9d690c9c7f16e
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-08 17:31:40 +0300

    paste contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link, Typography, Box } from "@mui/material";
+import { Link, Typography, Box, Divider } from "@mui/material";
 import Container from "@mui/material/Container";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import SmartphoneIcon from "@mui/icons-material/Smartphone";
+import FacebookIcon from "@mui/icons-material/Facebook";
 const Footer = () => {
   const thisYear = new Date().getFullYear();
   return (
@@ -16,10 +18,24 @@ const Footer = () => {
         bgcolor: "#d2fccf",
       }}
     >
-      <Box sx={{ width: "200px" }}>
-        <Typography variant="h5"> Booking Contact</Typography>
+      <Box
+        sx={{
+          height: "15vh",
+        }}
+      >
+        <Typography variant="h6"> Booking Contact: Mẹ Trần Như Hoa </Typography>
+        <Typography variant="body2">
+          {" "}
+          <SmartphoneIcon /> 0978 702552
+          <Divider />
+          <Link href="https://www.facebook.com/hoa.an.9003">
+            {" "}
+            <FacebookIcon />
+            Hoa Su Kien
+          </Link>{" "}
+        </Typography>
       </Box>
-      <Typography variant="body1">
+      <Typography variant="body2">
         &#169; {thisYear} Linh Nguyen - Write code with love!
         <Link
           href="https://github.com/linhngkh/xuka-thuyan"
