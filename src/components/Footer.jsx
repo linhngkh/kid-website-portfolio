0679b293b88c373c01e5de93d8139d47fab98d25
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-08 21:15:52 +0300

    polish styles for all components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Link, Typography, Box, Divider } from "@mui/material";
-import Container from "@mui/material/Container";
+import { Link, Typography, Box, Container } from "@mui/material";
+import PhoneCallbackIcon from "@mui/icons-material/PhoneCallback";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import SmartphoneIcon from "@mui/icons-material/Smartphone";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 const Footer = () => {
   const thisYear = new Date().getFullYear();
   return (
     <Container
-      maxWidth="lg"
+      maxWidth="2xl"
       sx={{
         display: "flex",
         alignItems: "center",
@@ -20,40 +21,61 @@ const Footer = () => {
     >
       <Box
         sx={{
-          height: "15vh",
+          height: "20vh",
+          display: "flex",
         }}
       >
-        <Box sx={{}}>
-          <Typography variant="h6">
+        {/* Nhu Hoa info */}
+        <Box sx={{ width: "50%", mr: "100px", margin: "20px" }}>
+          <Typography
+            variant="h6"
+            sx={{ lineHeight: "1.5", fontWeight: "bold" }}
+          >
             {" "}
             Booking Contact: Mẹ Trần Như Hoa{" "}
           </Typography>
           <Typography variant="body2">
             {" "}
-            <SmartphoneIcon /> 0978 702552{" "}
+            <PhoneCallbackIcon sx={{ verticalAlign: "middle" }} /> 0978 702552{" "}
           </Typography>
-        </Box>
-        <Box sx={{ display: "flex" }}>
           <Link
             href="https://www.facebook.com/hoa.an.9003"
-            sx={{ textDecoration: "none" }}
+            sx={{ textDecoration: "none", color: "black" }}
           >
             {" "}
-            <FacebookIcon />
+            <FacebookIcon sx={{ verticalAlign: "middle" }} />
             Hoa Su Kien
           </Link>{" "}
         </Box>
+        {/* xuka info */}
+        <Box sx={{ width: "60%", ml: "100px", margin: "20px" }}>
+          <Typography variant="h6">Xuka's Social Media</Typography>
+          <Link href="https://www.facebook.com/hoatran.event">
+            {" "}
+            <FacebookIcon sx={{ verticalAlign: "middle", color: "black" }} />
+          </Link>{" "}
+          <Link href="https://www.youtube.com/channel/UCCnspEmHAiDQSEwwONcbIMQ">
+            <YouTubeIcon
+              sx={{ verticalAlign: "middle", mr: "7px", color: "black" }}
+            />
+          </Link>
+          <Link href="https://www.facebook.com/hoatran.event">
+            <InstagramIcon sx={{ verticalAlign: "middle", color: "black" }} />
+          </Link>
+        </Box>
       </Box>
-      <Typography variant="body2">
-        &#169; {thisYear} Linh Nguyen - Write code with love!
-        <Link
-          href="https://github.com/linhngkh/xuka-thuyan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GitHubIcon style={{ color: "black" }} />
-        </Link>
-      </Typography>
+      <Box sx={{ position: "sticky" }}>
+        <Typography variant="body2">
+          &#169; {thisYear} Linh Nguyen - Write code with love!
+          <Link
+            href="https://github.com/linhngkh/xuka-thuyan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon style={{ color: "black" }} />
+          </Link>
+        </Typography>
+      </Box>
     </Container>
   );
 };
