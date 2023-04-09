2c2aa00f0cc54ae8fa95f32352bc3ac4a0bdbe5a
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-10 00:09:07 +0300

    made image collection com.

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { ImageList, ImageListItem } from "@mui/material";
 
+import image1 from "../assets/3G1A1424.jpeg";
+import image2 from "../assets/3G1A1426.jpeg";
 const Images = () => {
-  return <div>Images</div>;
+  const itemData = [image1, image2];
+
+  return (
+    <ImageList
+      sx={{ width: 500, height: 450, padding: "50px" }}
+      variant="woven"
+      cols={2}
+      gap={10}
+    >
+      {itemData.map((item, index) => (
+        <ImageListItem key={index}>
+          <img
+            src={`${item}?w=248&fit=crop&auto=format`}
+            srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
 };
 
 export default Images;
