162f170ffc3b3be286f208df341b577fb135f382
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-13 14:22:06 +0300

    edit css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,19 +10,19 @@ const Home = () => {
   return (
     <div style={{}}>
       <Parallax pages={4} style={{}}>
-        <ParallaxLayer offset={0} speed={1.5} factor={1}>
+        <ParallaxLayer offset={0} speed={1.0} factor={1}>
           <CarouselSlider />
         </ParallaxLayer>
-        <ParallaxLayer offset={0.8} speed={1.5} factor={1}>
+        <ParallaxLayer offset={0.8} speed={1.0} factor={1}>
           <Profile />
         </ParallaxLayer>
-        <ParallaxLayer offset={1.0} speed={1.5}>
+        <ParallaxLayer offset={1.0} speed={1.0}>
           <ParallaxText />
         </ParallaxLayer>
         <ParallaxLayer offset={1.2} speed={0.5} factor={1}>
           <Contact />
         </ParallaxLayer>
-        <ParallaxLayer offset={2} speed={2.5}>
+        <ParallaxLayer offset={2} speed={1.5}>
           {" "}
           <LogoSlider />
         </ParallaxLayer>
